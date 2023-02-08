19a13615270df7c325d16bbf650d92e3b2b4e0f3
DHANUSHPRAKASH V C <75581083+dhanush2006@users.noreply.github.com>
2023-02-08 18:26:00 +0530

    Update Of given 5 numbers, how many are even.java

--- Day-3/Of given 5 numbers, how many are even.java
+++ Day-3/Of given 5 numbers, how many are even.java
@@ -1,7 +1,10 @@
-public class IsMultiple{
-    public int isMultiple(int input1, int input2){
-        if (input1 == 0 || input2 == 0) return 3;
-        else if (input1 % input2 == 0) return 2;
-        else return 1;
+public class CountEvens{
+    public int countEvens(int input1, int input2, int input3, int  input4, int input5){
+        int [] arr = {input1, input2, input3, input4, input5};
+        int count = 0;
+        for (int num : arr){
+            if (num % 2 == 0) count++;
+        }
+        return count;
     }
 }
