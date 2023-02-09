c0c0775c2265611e5d6227915af6ffdadb9968ad
DHANUSHPRAKASH V C <75581083+dhanush2006@users.noreply.github.com>
2023-02-09 12:17:40 +0530

    Update Most Frequent Digit.java

--- Day-5/Number & string based problems/Most Frequent Digit.java
+++ Day-5/Number & string based problems/Most Frequent Digit.java
@@ -1,21 +1,17 @@
 import java.util.Arrays;
 public class MostFrequentDigit {
     public int getMostFrequestDigit(int input1, int input2, int input3, int input4) {
-        int [] nums = {input1, input2, input3, input4};
-        int [] freq = new int[10];
-        Arrays.fill(freq,0);
-
-        for (int num : nums){
-            while (num != 0){
-                int digit = num % 10;
-                freq[digit]++;
-                num /= 10;
-            }
-        }
-        int maxFreq = 0;
-        for (int i = 0; i<= 9; i++){
-            if (freq[maxFreq] <= freq[i]) maxFreq = i;
-        }
-        return maxFreq;
-    }
-}
+String allNo = String.valueOf(input1) + String.valueOf(input2) + String.valueOf(input3) + String.valueOf(input4);
+		int[] digitFrequency = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // 0-9
+		
+		for (int i = 0; i < allNo.length(); i++) {
+			digitFrequency[Integer.parseInt(String.valueOf(allNo.charAt(i)))]++;
+		}
+		
+		int mostFrequentDigit = 0;
+		
+		for (int i = 0; i <= 9; i++) {
+			mostFrequentDigit 	= digitFrequency[i] >= digitFrequency[mostFrequentDigit] ? i : mostFrequentDigit;
+		}
+		
+		return mostFrequentDigit;
