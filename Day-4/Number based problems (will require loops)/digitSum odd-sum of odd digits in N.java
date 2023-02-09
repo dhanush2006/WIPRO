d263be6a773966dc5f83281ec2901e4f230f285d
DHANUSHPRAKASH V C <75581083+dhanush2006@users.noreply.github.com>
2023-02-09 12:03:57 +0530

    Update digitSum odd-sum of odd digits in N.java

--- Day-4/Number based problems (will require loops)/digitSum odd-sum of odd digits in N.java
+++ Day-4/Number based problems (will require loops)/digitSum odd-sum of odd digits in N.java
@@ -1,16 +1,13 @@
-public class EvenOddDigitsSum {
-    public static int evenOddDigitsSum(int input1, String input2) {
-        int evenSum = 0;
-        int totalSum = 0;
+public class OddDigitsSum {
+    public static int oddDigitsSum(int input1) {
+        int sum = 0;
 
         while (input1 != 0) {
             int digit = input1 % 10;
-            totalSum += digit;
-            if (digit % 2 == 0) evenSum += digit;
+            if (digit % 2 == 1) sum += digit;
             input1 /= 10;
         }
 
-        if (input2.equals("even")) return evenSum;
-        else return totalSum - evenSum;
+        return sum;
     }
 }
